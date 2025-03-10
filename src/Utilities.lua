5bca542df591325716a5a0c2934bce6b9f5c9fbc
LxckStxp <lxckstxp@gmail.com>
2025-03-10 10:40:57 +1000

    Update Utilities.lua

--- src/Utilities.lua
+++ src/Utilities.lua
@@ -1,1 +1,12 @@
+-- Utilities.lua: Reusable helper functions
+local Utilities = {}
 
+function Utilities.createInstance(className, properties)
+    local instance = Instance.new(className)
+    for prop, value in pairs(properties or {}) do
+        instance[prop] = value
+    end
+    return instance
+end
+
+return Utilities
