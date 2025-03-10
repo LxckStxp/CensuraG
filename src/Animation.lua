4d7b945f3835692f1b36ef6604b777e1d5f9df6d
LxckStxp <lxckstxp@gmail.com>
2025-03-10 12:57:28 +1000

    Update Animation.lua

--- src/Animation.lua
+++ src/Animation.lua
@@ -1,26 +1,15 @@
--- Animation.lua: Simple animation utilities with improved consistency
+-- Animation.lua: Simple animation utilities
 local Animation = {}
 local TweenService = game:GetService("TweenService")
 local logger = _G.CensuraG.Logger
 
-Animation.TweenQueue = {}
-
 function Animation:Tween(element, properties, duration, callback)
     local tweenInfo = TweenInfo.new(duration or 0.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
     local tween = TweenService:Create(element, tweenInfo, properties)
-    table.insert(self.TweenQueue, {tween = tween, callback = callback})
-    logger:debug("Queued tween for element: %s, Properties: %s", tostring(element), tostring(properties))
-
-    tween.Completed:Connect(function()
-        local index = table.find(self.TweenQueue, {tween = tween})
-        if index then
-            table.remove(self.TweenQueue, index)
-            if callback then
-                pcall(callback)
-                logger:debug("Completed tween for element: %s", tostring(element))
-            end
-        end
-    end)
+    logger:debug("Started tween for element: %s, Properties: %s", tostring(element), tostring(properties))
+    if callback then
+        tween.Completed:Connect(callback)
+    end
     tween:Play()
     return tween
 end
@@ -35,13 +24,6 @@ function Animation:HoverEffect(element)
         element.BorderSizePixel = 0
         logger:debug("Hover effect removed from element: %s", tostring(element))
     end)
-end
-
-function Animation:WaitForAnimations()
-    while #self.TweenQueue > 0 do
-        wait(0.1) -- Wait for animations to complete
-    end
-    logger:debug("All animations completed")
 end
 
 return Animation
