46b177f7ae2866b6846daf7eb982360135931dec
LxckStxp <lxckstxp@gmail.com>
2025-03-10 15:19:41 +1000

    Update ImageLabel.lua

--- src/Elements/ImageLabel.lua
+++ src/Elements/ImageLabel.lua
@@ -15,33 +15,44 @@ function ImageLabel.new(parent, imageUrl, x, y, width, height, options)
 
     logger:debug("Creating ImageLabel with parent: %s, ImageURL: %s, Position: (%d, %d)", tostring(parent.Instance), imageUrl, x, y)
 
-    local image = Utilities.createInstance("ImageLabel", {
+    -- Create the main frame
+    local frame = Utilities.createInstance("Frame", {
         Parent = parent.Instance,
         Position = UDim2.new(0, x, 0, y),
         Size = UDim2.new(0, width or 50, 0, height or 50),
         BackgroundTransparency = 1,
+        ZIndex = parent.Instance.ZIndex + 1
+    })
+
+    -- Create the image
+    local image = Utilities.createInstance("ImageLabel", {
+        Parent = frame,
+        Position = UDim2.new(0, 0, 0, 0),
+        Size = UDim2.new(0, width or 50, 0, height or 50),
+        BackgroundTransparency = 1,
         Image = imageUrl or "",
         Visible = true,
-        ZIndex = options.ZIndex or 3
+        ZIndex = frame.ZIndex + 1
     })
     Styling:Apply(image, "ImageLabel")
     logger:debug("ImageLabel created: Position: %s, Size: %s, ZIndex: %d", tostring(image.Position), tostring(image.Size), image.ZIndex)
 
     local self = setmetatable({
-        Instance = image
+        Instance = frame,
+        Image = image
     }, ImageLabel)
 
+    function self:SetImage(url)
+        self.Image.Image = url
+        logger:debug("Updated ImageLabel image to: %s", url)
+    end
+
+    function self:Destroy()
+        self.Instance:Destroy()
+        logger:info("ImageLabel destroyed")
+    end
+
     return self
-end
-
-function ImageLabel:SetImage(url)
-    self.Instance.Image = url
-    logger:debug("Updated ImageLabel image to: %s", url)
-end
-
-function ImageLabel:Destroy()
-    self.Instance:Destroy()
-    logger:info("ImageLabel destroyed")
 end
 
 return ImageLabel
