b4db06b62d868bcb310230b5b72d202590684357
LxckStxp <lxckstxp@gmail.com>
2025-03-10 10:41:08 +1000

    Update UIElement.lua

--- src/UIElement.lua
+++ src/UIElement.lua
@@ -1,1 +1,19 @@
+-- UIElement.lua: Base class for all UI elements
+local UIElement = {}
+UIElement.__index = UIElement
 
+function UIElement.new(instance)
+    local self = setmetatable({}, UIElement)
+    self.Instance = instance
+    return self
+end
+
+function UIElement:SetPosition(x, y)
+    self.Instance.Position = UDim2.new(0, x, 0, y)
+end
+
+function UIElement:SetSize(width, height)
+    self.Instance.Size = UDim2.new(0, width, 0, height)
+end
+
+return UIElement
