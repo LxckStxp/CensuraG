9ba6387a60ee3307fb0fb138b1f4d719f062f3e0
LxckStxp <lxckstxp@gmail.com>
2025-03-10 13:51:15 +1000

    Update Animation.lua

--- src/Animation.lua
+++ src/Animation.lua
@@ -4,13 +4,13 @@ local TweenService = game:GetService("TweenService")
 local logger = _G.CensuraG.Logger
 
 function Animation:SlideY(element, targetY, duration, easingStyle, easingDirection, callback)
-    local easingStyle = easingStyle or Enum.EasingStyle.Linear
-    local easingDirection = easingDirection or Enum.EasingDirection.InOut
-    local tweenInfo = TweenInfo.new(duration or 0.3, easingStyle, easingDirection)
+    local style = easingStyle or Enum.EasingStyle.Linear
+    local direction = easingDirection or Enum.EasingDirection.InOut
+    local tweenInfo = TweenInfo.new(duration or 0.3, style, direction)
     local currentPosition = element.Position
     local targetPosition = UDim2.new(currentPosition.X.Scale, currentPosition.X.Offset, 0, targetY)
     local tween = TweenService:Create(element, tweenInfo, {Position = targetPosition})
-    logger:debug("Started Y-axis slide for element: %s, Target Y: %d, Easing: %s, Direction: %s", tostring(element), targetY, tostring(easingStyle), tostring(easingDirection))
+    logger:debug("Started Y-axis slide for element: %s, Target Y: %d, Easing: %s, Direction: %s", tostring(element), targetY, tostring(style), tostring(direction))
     if callback then
         tween.Completed:Connect(callback)
     end
