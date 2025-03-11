53980bea423bcec43b8a7b03f32a90a0a72ec759
LxckStxp <lxckstxp@gmail.com>
2025-03-12 00:15:30 +1000

    Update textlabel.lua

--- src/components/textlabel.lua
+++ src/components/textlabel.lua
@@ -10,6 +10,12 @@ return function(parent, text)
     Label.TextColor3 = theme.TextColor
     Label.Font = theme.Font
     Label.TextSize = theme.TextSize
+    Label.TextTransparency = 1 -- Start invisible
+    
+    -- Fade-in animation
+    _G.CensuraG.AnimationManager:Tween(Label, {
+        TextTransparency = 0
+    }, Config.Animations.FadeDuration)
     
     _G.CensuraG.Logger:info("TextLabel created with text: " .. text)
     return Label
