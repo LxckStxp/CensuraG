07181c6a405555829cbb7d9d76641dcab316cde5
LxckStxp <lxckstxp@gmail.com>
2025-03-12 00:21:45 +1000

    Update textlabel.lua

--- src/components/textlabel.lua
+++ src/components/textlabel.lua
@@ -3,6 +3,8 @@ local Config = _G.CensuraG.Config
 
 return function(parent, text)
     local theme = Config:GetTheme()
+    local animConfig = Config.Animations
+    
     local Label = Instance.new("TextLabel", parent)
     Label.Size = UDim2.new(1, 0, 0, 20)
     Label.BackgroundTransparency = 1
@@ -10,13 +12,18 @@ return function(parent, text)
     Label.TextColor3 = theme.TextColor
     Label.Font = theme.Font
     Label.TextSize = theme.TextSize
-    Label.TextTransparency = 1 -- Start invisible
+    Label.TextTransparency = 1 -- Start hidden
     
-    -- Fade-in animation
-    _G.CensuraG.AnimationManager:Tween(Label, {
-        TextTransparency = 0
-    }, Config.Animations.FadeDuration)
+    -- Animation
+    _G.CensuraG.AnimationManager:Tween(Label, {TextTransparency = 0}, animConfig.FadeDuration)
+    
+    local TextLabel = {
+        Instance = Label,
+        Refresh = function(self)
+            _G.CensuraG.Methods:RefreshComponent("textlabel", self.Instance)
+        end
+    }
     
     _G.CensuraG.Logger:info("TextLabel created with text: " .. text)
-    return Label
+    return TextLabel.Instance -- Return instance for compatibility
 end
