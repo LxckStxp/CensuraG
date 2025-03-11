981bd15fdd8cd77760698afc7afe726ebe6a8623
LxckStxp <lxckstxp@gmail.com>
2025-03-12 00:15:51 +1000

    Update imagelabel.lua

--- src/components/imagelabel.lua
+++ src/components/imagelabel.lua
@@ -2,11 +2,18 @@
 local Config = _G.CensuraG.Config
 
 return function(parent, imageId)
+    local theme = Config:GetTheme()
     local Image = Instance.new("ImageLabel", parent)
     Image.Size = UDim2.new(0, 50, 0, 50)
     Image.BackgroundTransparency = 1
-    Image.Image = imageId or "rbxassetid://0" -- Default to empty if no ID provided
+    Image.Image = imageId or "rbxassetid://0"
     Image.BorderSizePixel = 0
+    Image.ImageTransparency = 1 -- Start invisible
+    
+    -- Fade-in animation
+    _G.CensuraG.AnimationManager:Tween(Image, {
+        ImageTransparency = 0
+    }, Config.Animations.FadeDuration)
     
     _G.CensuraG.Logger:info("ImageLabel created with ID: " .. (imageId or "none"))
     return Image
